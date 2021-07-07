00ffabf67574955789bf9b741fb99d3c979f8f4b
Illia <ilya@sheremetov.com>
2021-07-07 14:07:15 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '980706187b0659768ef60d05053e58fd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d4c517a-9a8b-479e-8800-0169e625480d'
+};
